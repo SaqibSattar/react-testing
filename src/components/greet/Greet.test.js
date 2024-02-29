8daf6333a63be45ff10d8d2fa6b68c5a8f41463e
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-29 12:03:14 +0400

    Grouping Tests

--- src/components/greet/Greet.test.js
+++ src/components/greet/Greet.test.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Greet } from './Greet.tsx';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./Greet.tsx";
 
-test('Greet render correctly', () => {
+// describe.skip("Greet", () => {
+describe("Greet", () => {
+  test("render correctly", () => {
     render(<Greet />);
     const textElement = screen.getByText(/hello/i);
     expect(textElement).toBeInTheDocument();
   });
+});
 
-  test("Greet renders with a name", () => {
+describe("Nested", () => {
+  test("renders with a name", () => {
     render(<Greet name="Saqi" />);
     const textElement = screen.getByText("Hello Saqi");
     expect(textElement).toBeInTheDocument();
   });
+});
