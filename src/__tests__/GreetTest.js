b0d80e41c14342a9c7f9336308c15660abad24b9
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-29 12:14:45 +0400

    jest filename conventions

---
+++ src/__tests__/GreetTest.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+import { Greet } from "../components/greet/Greet.tsx";
+
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("renders a name", () => {
+    render(<Greet name="Saqi" />);
+    const textElement = screen.getByText("Hello Saqi");
+    expect(textElement).toBeInTheDocument();
+  });
+});
---
+++ src/components/greet/Greet.spec.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./Greet.tsx";
+
+// describe.skip("Greet", () => {
+describe("Greet", () => {
+  test("render correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+});
+
+describe("Nested", () => {
+  test("renders with a name", () => {
+    render(<Greet name="Saqi" />);
+    const textElement = screen.getByText("Hello Saqi");
+    expect(textElement).toBeInTheDocument();
+  });
+});
+
+// it alternative to test
+// xit alternative to test.skip
+// fit alternative to test.only
