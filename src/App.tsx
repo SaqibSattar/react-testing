d52593bc3462638b2879dc43c26ed81b5ae1179f
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-02 11:02:00 +0400

    Query multiple elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Application } from "./components/application/Application";
 import './App.css'
+import { Skills } from "./components/skills/Skills";
 
 function App() {
 
   return (
     <div>
       <Application />
+      <Skills skills={["HTML", "CSS"]} />
     </div>
   )
 }
