23b09d82a3e9979462c4a06bad38cfb06bdde245
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-02 12:24:01 +0400

    Providers

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import { Application } from "./components/application/Application";
 import './App.css'
 import { Skills } from "./components/skills/Skills";
 import { Counter } from "./components/counter/Counter";
+import { AppProviders } from "./providers/AppProviders";
+import { MuiMode } from "./components/mui/MuiMode";
 
 function App() {
 
   return (
-    <div>
-      <Application />
-      <Skills skills={["HTML", "CSS"]} />
-      <Counter />
-    </div>
+    <AppProviders>
+      <div className="App">
+        <Application />
+        <Skills skills={["HTML", "CSS"]} />
+        <Counter />
+        <MuiMode />
+      </div>
+    </AppProviders>
   )
 }
 
