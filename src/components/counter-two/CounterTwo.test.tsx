3cdc11eb639d4dddd6697364919d783e59c2cd32
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-02 13:25:56 +0400

    update mocking functions to use user-event

--- src/components/counter-two/CounterTwo.test.tsx
+++ src/components/counter-two/CounterTwo.test.tsx
@@ -1,22 +1,24 @@
 import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
 import { CounterTwo } from "./CounterTwo";
 
 describe("CounterTwo", () => {
   test("renders correctly", () => {
-    const incrementHandler = jest.fn();
-    const decrementHandler = jest.fn();
-    render(
-      <CounterTwo
-        count={0}
-        handleIncrement={incrementHandler}
-        handleDecrement={decrementHandler}
-      />
-    );
+    // const incrementHandler = jest.fn();
+    // const decrementHandler = jest.fn();
+    // render(
+    //   <CounterTwo
+    //     count={0}
+    //     handleIncrement={incrementHandler}
+    //     handleDecrement={decrementHandler}
+    //   />
+    // );
+    render(<CounterTwo count={0} />);
     const textElement = screen.getByText("Counter Two");
     expect(textElement).toBeInTheDocument();
   });
 
-  test("handlers are called", () => {
+  test("handlers are called", async () => {
     const incrementHandler = jest.fn();
     const decrementHandler = jest.fn();
     render(
@@ -26,10 +28,10 @@ describe("CounterTwo", () => {
         handleDecrement={decrementHandler}
       />
     );
-    const incrementButton = screen.getByText("Increment");
-    const decrementButton = screen.getByText("Decrement");
-    incrementButton.click();
-    decrementButton.click();
+    const incrementButton = screen.getByRole("button", { name: "Increment" });
+    const decrementButton = screen.getByRole("button", { name: "Decrement" });
+    await user.click(incrementButton);
+    await user.click(decrementButton);
     expect(incrementHandler).toHaveBeenCalledTimes(1);
     expect(decrementHandler).toHaveBeenCalledTimes(1);
   });
